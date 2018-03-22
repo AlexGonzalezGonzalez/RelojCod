b045154ddc0825784cb0f0d786d0155a64f07cd2
estudios <estudios@192.168.0.25>
2018-03-22 08:54:03 +0100

    Reloj finalizado

--- src/reloj/Reloj.java
+++ src/reloj/Reloj.java
@@ -12,43 +12,50 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author estudios
+ * @author Alejandro
  */
 public class Reloj {
-
+    
     int hora;
     int minuto;
     int segundo;
     Timer timer;
     private int horas, minutos, segundos;
 
+    /**
+     *Constructor del reloj donde se crea un objeto timer y la accion de este es el timer task, con 0 delay(retardo) y se repite cada segundo.
+     */
     public Reloj() {
-
         timer = new Timer();
-
-        timer.schedule(new TicTac(), 0, 1000);
+        timer.schedule(new Tiempo(), 0, 1000);
     }
 
-    public class TicTac extends TimerTask {
+    /**
+     *Clase Tiempo que coge la hora del sistema y la compara con la alarma, si es igual se muestra el mensaje ALARMA, sino, se muestra la hora cada segundo.
+     */
+    public class Tiempo extends TimerTask {
 
+        /**
+         *Metodo run del timerTask, es el metodo que se ejecuta con el tiempo predefinido que le hemos puesto en el constructor.
+         */
         public void run() {
-
             Date date = new Date();
+            
             horas = date.getHours();
             minutos = date.getMinutes();
             segundos = date.getSeconds();
-
             if (horas == hora && minutos == minuto && segundos == segundo) {
                 System.out.println("ALARMA");
             } else {
                 System.out.println(horas + ":" + minutos + ":" + segundos);
             }
         }
-
     }
 
+    /**
+     *Metodo alarma donde se piden las variables hora,minuto y segundo 
+     */
     public void alarma() {
-
         hora = Integer.parseInt(JOptionPane.showInputDialog("alarma"));
         minuto = Integer.parseInt(JOptionPane.showInputDialog("alarma"));
         segundo = Integer.parseInt(JOptionPane.showInputDialog("alarma"));
